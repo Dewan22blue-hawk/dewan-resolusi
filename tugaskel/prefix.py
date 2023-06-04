7fde7026fea053c16b9df9574f3657a439f8d259
Dewan22blue-hawk <104411395+Dewan22blue-hawk@users.noreply.github.com>
2023-06-04 21:31:51 +0700

    aku titipkan dia lanjutkan perjuangan ku tuk nyA

---
+++ tugaskel/prefix.py
@@ -0,0 +1,48 @@
+def infix_to_prefix(infix_expr):
+    # Membalikkan string infix
+    infix_expr = infix_expr[::-1]
+
+    # Membuat kamus untuk presedensi operator
+    operators = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+
+    # Membuat tumpukan dan daftar untuk menyimpan hasil konversi
+    stack = []
+    prefix_expr = []
+
+    # Melakukan iterasi pada setiap karakter dalam ekspresi infix
+    for char in infix_expr:
+        # Jika karakter adalah operand, tambahkan ke hasil konversi
+        if char.isalnum():
+            prefix_expr.append(char)
+        # Jika karakter adalah tanda kurung tutup, masukkan ke dalam tumpukan
+        elif char == ')':
+            stack.append(char)
+        # Jika karakter adalah tanda kurung buka, keluarkan operator dari tumpukan
+        elif char == '(':
+            while stack and stack[-1] != ')':
+                prefix_expr.append(stack.pop())
+            stack.pop()
+        # Jika karakter adalah operator, keluarkan operator dengan presedensi yang lebih tinggi dari tumpukan
+        else:
+            while stack and operators.get(stack[-1], 0) > operators.get(char, 0):
+                prefix_expr.append(stack.pop())
+            stack.append(char)
+
+    # Menambahkan operator yang tersisa dari tumpukan ke hasil konversi
+    while stack:
+        prefix_expr.append(stack.pop())
+
+    # Membalikkan hasil konversi untuk mendapatkan ekspresi prefix yang benar
+    prefix_expr = prefix_expr[::-1]
+
+    # Menggabungkan semua karakter menjadi satu string
+    prefix_expr = ''.join(prefix_expr)
+
+    return prefix_expr
+
+
+# Contoh penggunaan program
+while True :
+    infix_expression = input("Masukkan ekspresi infix: ")
+    prefix_expression = infix_to_prefix(infix_expression)
+    print("Ekspresi prefix: ", prefix_expression)
